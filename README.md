1b23b790463873df6daba3c1c7c252741be45494
Your Name <you@example.com>
2022-07-03 04:12:24 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# FilmIt" 
+"# FilmIt" 
---
+++ utils/client.ts
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: '451r9o4j',
+  dataset: 'production',
+  apiVersion: '2022-03-10',
+  useCdn: false,
+  token: 'skKGtTklBr08G9MVKZvnYDoOStJh6NNP2TggucHUR7NSrwQgmr6ygW01eKrjlyDtQS7T2HQMkxe5qwmM6cS9c02XqipJL20PByJaYvryTYOrmjj1Cy2U7tVoN39sTA7oy2utO1eATSvj8ZPXDJgOnVPKARULGy3RH1sN18yA8OYTTyT7K5tx',
+});
